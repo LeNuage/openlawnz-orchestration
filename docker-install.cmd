2c8016c3620d2daeaf1a92a92f2c83127b8f7264
openlawnz-alex <55544499+openlawnz-alex@users.noreply.github.com>
2019-10-18 15:13:02 +0800

    Add files via upload
    Create scripts to install postgres db

---
+++ docker-install.cmd
@@ -0,0 +1,24 @@
+ECHO OFF
+SET DB_MOUNT_VOLUME=data
+
+ECHO "Creating mount volume directory"
+md %DB_MOUNT_VOLUME%
+
+REM Docker machines may have new IP addresses. You may need to re-run the `docker-machine env` command
+
+ECHO "Running docker image"
+docker pull postgres
+docker run --name openlawnz-postgres -p5432:5432 -v %DB_MOUNT_VOLUME%:/var/lib/postgresql/data -d postgres
+
+ECHO "Downloading latest OpenLaw NZ database"
+REM curl -o openlawnzdb.sql https:<URL provided by volunteering with OpenLaw NZ>
+
+ECHO "Copying database into docker"
+docker cp openlawnzdb.sql openlawnz-postgres:/tmp
+
+ECHO "Restoring database into postgres instance" 
+docker exec -it openlawnz-postgres psql -U postgres -c "create database openlawnz_db"
+docker exec -it openlawnz-postgres pg_restore --no-owner -U postgres -d openlawnz_db /tmp/openlawnzdb.sql  -v
+
+docker exec openlawnz-postgres rm /tmp/openlawnzdb.sql
+rm openlawnzdb.sql
